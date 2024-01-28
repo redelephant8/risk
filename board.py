c43e475f2acea6a2423f95a36f6030c83f4afbff
Orian Gavish <80489815+redelephant8@users.noreply.github.com>
2024-01-28 12:48:14 +0200

    Update board.py

--- board.py
+++ board.py
@@ -5,23 +5,20 @@ from territory import Territory
 
 class Board:
     def __init__(self):
-        self.pieces = {}
+        self.territories = {}
         self.initialize_territories()
         self.setup_neighbors()
 
     def initialize_territories(self):
-        self.pieces["qatar"] = Territory("Qatar", 50, 100, "qatar.png")
-        self.pieces["egypt"] = Territory("Egypt", 100, 150, "egypt.png")
-        self.pieces["iran"] = Territory("Iran", 150, 200, "iran.png")
-        self.pieces["saudi_arabia"] = Territory("Saudi Arabia", 200, 250, "saudi_arabia.png")
-        self.pieces["afghanistan"] = Territory("Afghanistan", 250, 300, "afghanistan.png")
+        self.territories["qatar"] = Territory("Qatar", 50, 100, "qatar.png")
+        self.territories["egypt"] = Territory("Egypt", 100, 500, "egypt.png")
+        self.territories["iran"] = Territory("Iran", 150, 200, "iran.png")
+        self.territories["saudi_arabia"] = Territory("Saudi Arabia", 200, 500, "saudi_arabia.png")
+        self.territories["afghanistan"] = Territory("Afghanistan", 250, 400, "afghanistan.png")
 
     def setup_neighbors(self):
-        self.pieces["qatar"].neighbors = [self.pieces["egypt"], self.pieces["iran"], self.pieces["afghanistan"]]
-        self.pieces["egypt"].neighbors = [self.pieces["qatar"], self.pieces["iran"], self.pieces["saudi_arabia"]]
-        self.pieces["saudi_arabia"].neighbors = [self.pieces["egypt"], self.pieces["afghanistan"]]
-        self.pieces["afghanistan"].neighbors = [self.pieces["saudi_arabia"], self.pieces["qatar"]]
-        self.pieces["iran"].neighbors = [self.pieces["qatar"], self.pieces["egypt"]]
-
-
-
+        self.territories["qatar"].neighbors = [self.territories["egypt"], self.territories["iran"], self.territories["afghanistan"]]
+        self.territories["egypt"].neighbors = [self.territories["qatar"], self.territories["iran"], self.territories["saudi_arabia"]]
+        self.territories["saudi_arabia"].neighbors = [self.territories["egypt"], self.territories["afghanistan"]]
+        self.territories["afghanistan"].neighbors = [self.territories["saudi_arabia"], self.territories["qatar"]]
+        self.territories["iran"].neighbors = [self.territories["qatar"], self.territories["egypt"]]
