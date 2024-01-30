d233f833b912c03cecddafbd52552d30c7c8f15d
Orian Gavish <80489815+redelephant8@users.noreply.github.com>
2024-01-30 12:46:14 +0200

    Update territory.py

--- territory.py
+++ territory.py
@@ -1,4 +1,7 @@
 import pygame
+pygame.init()
+pygame.font.init()
+
 
 class Territory:
     def __init__(self, name, x, y, image_location):
@@ -9,9 +12,15 @@ class Territory:
         original_image = pygame.image.load(image_location)
         self.image = pygame.transform.scale(original_image, (100, 100))
         self.rect = self.image.get_rect(topleft=(x, y))
-
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
+        self.coordinates = (x, y)
 
     def click(self, mouse_pos):
         return self.rect.collidepoint(mouse_pos)
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+        pygame.draw.rect(screen, (255, 0, 0) if self.owner is None else self.owner.color,
+                         (self.coordinates[0], self.coordinates[1], 50, 50))
+        font = pygame.font.Font(None, 36)
+        text = font.render(str(self.soldierNumber), True, (0, 0, 0))
+        screen.blit(text, (self.coordinates[0] + 10, self.coordinates[1] + 10))
