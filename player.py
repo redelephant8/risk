87cfe02648b633a52342ee22d44ed5950fa137a9
Orian Gavish <80489815+redelephant8@users.noreply.github.com>
2024-02-13 12:50:56 +0200

    Update player.py

--- player.py
+++ player.py
@@ -1,12 +1,20 @@
 from board import Board
 
 class Player:
-    def __init__(self, color, name, position=None):
+    def __init__(self, color, name, soldiers_in_hand=3):
         self.name = name
         self.color = color
         self.territoryNumber = 0
         self.territories = []
         self.cards = []
+        self.soldiers_in_hand = soldiers_in_hand
+        self.isOut = False
+
+    def reinforcement_calculator(self):
+        if self.soldiers_in_hand < 11:
+            return 3
+        return self.soldiers_in_hand % 3
+
 
 
 
