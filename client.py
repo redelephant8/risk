356c62faa5139478baf1b008f479123c024a6092
Orian Gavish <80489815+redelephant8@users.noreply.github.com>
2024-04-04 15:28:13 +0300

    Update client.py

--- client.py
+++ client.py
@@ -328,7 +328,7 @@ class RiskClient:
 
 
 if __name__ == "__main__":
-    HOST = "192.168.86.148"  # Change this to your server's IP address
+    HOST = "10.116.3.115"  # Change this to your server's IP address
     PORT = 8080  # Choose the same port as the server
     # player_name = input("Enter your player name: ")
     client = RiskClient(HOST, PORT)
