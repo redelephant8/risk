26f6574d5c6873a37da893ceab89df4248fd2097
Orian Gavish <80489815+redelephant8@users.noreply.github.com>
2024-04-21 22:04:31 +0300

    Added cards

--- player.py
+++ player.py
@@ -1,20 +1,21 @@
 from board import Board
 
 class Player:
-    def __init__(self, color, name, connection, soldiers_in_hand=3):
+    def __init__(self, color, name, connection, soldiers_in_hand=5):
         self.name = name
         self.color = color
         self.territoryNumber = 0
         self.territories = []
-        self.cards = []
+        self.cards = {"infantry": 0, "cavalry": 0, "artillery": 0}
+        self.has_conquered = False
         self.soldiers_in_hand = soldiers_in_hand
         self.isOut = False
         self.connection = connection
 
     def reinforcement_calculator(self):
-        if self.soldiers_in_hand < 11:
+        if len(self.territories) < 11:
             return 3
-        return self.soldiers_in_hand % 3
+        return len(self.territories) % 3
 
 
 
